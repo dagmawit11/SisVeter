5d9df2989efcf7a2cdbbf359d124b3c6954e79b1
Dagmawit Belay <36074244+dagmawit11@users.noreply.github.com>
2026-07-09 22:12:34 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 from decouple import config
-import dj_database_url
+import dj_database_url # pyright: ignore[reportMissingImports]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
