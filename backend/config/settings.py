a66d02e29a35199f39d90e653c7265adcac3a964
Dagmawit Belay <36074244+dagmawit11@users.noreply.github.com>
2026-07-09 22:00:44 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
-    }
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
