c111dc18ac3b9331992eabadd42af4f8365fd514
Dagmawit Belay <36074244+dagmawit11@users.noreply.github.com>
2026-07-10 21:22:13 +0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-
-    baseURL: "http://127.0.0.1:8000/api/"
-
+export default axios.create({
+  baseURL: "https://sisveter.onrender.com/api/",
 });
-
-export default api;
